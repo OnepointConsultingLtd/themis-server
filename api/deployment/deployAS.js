81222953d844637b5eab2e4f090001b4ebfb8f3e
Sotirios Angelopoulos <sotiris.angelopoulos@gmail.com>
2021-09-15 17:33:06 +0300

    updating dependencies

--- api/deployment/deployAS.js
+++ api/deployment/deployAS.js
@@ -10,47 +10,49 @@ const account = process.env.RULES_AZURE_ACCOUNT;
 const sas = process.env.RULES_AZURE_SAS;
 const containerName = process.env.RULES_AZURE_CONTAINER;
 
-const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net${sas}`);
+const blobServiceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net${sas}`
+);
 
 async function uploadBlob(content, fileName) {
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blobName = `${timestamp.utc("YYYYMMDDHHmmss")}_${fileName}`;
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  console.log(content.length);
-  let blobList = [];
-  try {
-    const uploadRes = await blockBlobClient.upload(content, content.length);
-    console.log(uploadRes);
-    const blobs = containerClient.listBlobsFlat();
-    for await (const blob of blobs) {
-      console.log(blob.name);
-      blobList.push(blob.name);
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobName = `${timestamp.utc("YYYYMMDDHHmmss")}_${fileName}`;
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    console.log(content.length);
+    let blobList = [];
+    try {
+        const uploadRes = await blockBlobClient.upload(content, content.length);
+        console.log(uploadRes);
+        const blobs = containerClient.listBlobsFlat();
+        for await (const blob of blobs) {
+            console.log(blob.name);
+            blobList.push(blob.name);
+        }
+    } catch (err) {
+        console.error(err);
+        return false;
     }
-  } catch (err) {
-    console.error(err);
+    console.log(blobList);
+    if (blobList.includes(blobName)) {
+        console.log("Found file in AazureStorage!", blobName);
+        return true;
+    }
     return false;
-  }
-  console.log(blobList);
-  if (blobList.includes(blobName)) {
-    console.log("Found file in AazureStorage!", blobName);
-    return true;
-  }
-  return false;
 }
 
 /** receive and upload rules dslr text file */
 app.post(url.deploy.azure, async (req, res) => {
-  const uploadResponse = await uploadBlob(req.body, req.params.fileName);
-  console.log("Return: ", uploadResponse);
-  // if (err /* || stderr */) res.status(404).send({ message: `${err} ${stderr}` }); // TODO: we need to get feedback from Azure Storage
-  if (!uploadResponse)
-    res.status(500).send({
-      message: "File was not deployed",
-    });
-  else
-    res.status(200).send({
-      response: "File was succesfully deployed!",
-    });
+    const uploadResponse = await uploadBlob(req.body, req.params.fileName);
+    console.log("Return: ", uploadResponse);
+    // if (err /* || stderr */) res.status(404).send({ message: `${err} ${stderr}` }); // TODO: we need to get feedback from Azure Storage
+    if (!uploadResponse)
+        res.status(500).send({
+            message: "File was not deployed",
+        });
+    else
+        res.status(200).send({
+            response: "File was succesfully deployed!",
+        });
 });
 
 module.exports = app;
